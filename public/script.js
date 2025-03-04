3be73358b235a7b237fb77d426f807388e1d2337
meherab111 <mehrabborno@gmail.com>
2025-03-04 23:37:42 +0600

    updated

--- public/script.js
+++ public/script.js
@@ -1,108 +1,99 @@
+let loadBody = document.body;
 
-let loadBody = document.body
+let inputFieldForm = document.querySelector(".input_field_form");
 
-let inputFieldForm = document.querySelector(".input_field_form")
+let inputField = document.querySelector(".input_field");
 
-let inputField = document.querySelector(".input_field")
+let weatherAddress = document.querySelector(".weather_address");
 
-let weatherAddress = document.querySelector(".weather_address")
+let weatherDate = document.querySelector(".weather_date");
 
-let weatherDate = document.querySelector(".weather_date")
+let weatherIcon = document.querySelector(".weather_icon_div");
 
-let weatherIcon = document.querySelector(".weather_icon_div")
+let weatherTemp = document.querySelector(".weather_temp");
 
-let weatherTemp = document.querySelector(".weather_temp")
+let weatherMin = document.querySelector(".weather_min");
 
-let weatherMin = document.querySelector(".weather_min")
+let weatherMax = document.querySelector(".weather_max");
 
-let weatherMax = document.querySelector(".weather_max")
+let weatherFeelsLike = document.querySelector(".weather_feels_like");
 
-let weatherFeelsLike = document.querySelector(".weather_feels_like")
+let weatherHumidity = document.querySelector(".weather_humidity");
 
-let weatherHumidity = document.querySelector(".weather_humidity")
+let weatherWind = document.querySelector(".weather_wind");
 
-let weatherWind = document.querySelector(".weather_wind")
+let weatherPressure = document.querySelector(".weather_pressure");
 
-let weatherPressure = document.querySelector(".weather_pressure")
-
-let city = "dhaka"
-
+let city = "dhaka";
 
 // search city
 
 inputFieldForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    e.preventDefault()
+  city = inputField.value;
 
-   city = inputField.value
-    
-   fetchApiDataFunc()
+  fetchApiDataFunc();
 
-   inputField.value = ""
-
-})
-
+  inputField.value = "";
+});
 
 // fetch Functionality
 
 const fetchApiDataFunc = async () => {
+  const fetchedApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e687b23172c6437d96093a197c9e283e`;
 
-    const fetchedApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e687b23172c6437d96093a197c9e283e`
+  try {
+    let result = await fetch(fetchedApi);
+    let fetchedData = await result.json();
 
-    try {
-        
-    let result = await fetch(fetchedApi)  
-    let fetchedData = await result.json()
-    
-    const {main, name, sys, weather, wind, dt} = fetchedData
+    const { main, name, sys, weather, wind, dt } = fetchedData;
 
-// using Intl api to display Country Full Name
-    
-    const regionNamesInEnglish = new Intl.DisplayNames([sys.country], { type: 'region' });
-    weatherAddress.textContent = `${name}, ${regionNamesInEnglish.of(sys.country)}`
-        
+    // using Intl api to display Country Full Name
 
-// using Intl api to display Date and Time
+    const regionNamesInEnglish = new Intl.DisplayNames([sys.country], {
+      type: "region",
+    });
+    weatherAddress.textContent = `${name}, ${regionNamesInEnglish.of(
+      sys.country
+    )}`;
 
-    let dateTime =  new Date(dt*1000)
+    // using Intl api to display Date and Time
+
+    let dateTime = new Date(dt * 1000);
     const dateTimeObj = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    const dateInFullFormat = new Intl.DateTimeFormat("en-US", dateTimeObj);
+    weatherDate.textContent = dateInFullFormat.format(dateTime);
 
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
+    // displaying all the info
 
-    }
-    const dateInFullFormat = new Intl.DateTimeFormat("en-US",dateTimeObj);
-     weatherDate.textContent = dateInFullFormat.format(dateTime)
+    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" /> <p class="W_stat">${weather[0].main}</p>`;
 
+    weatherTemp.innerHTML = `${(main.temp - 273.15).toFixed(2)}&#176`;
 
-// displaying all the info
+    weatherMin.innerHTML = `Min : ${(main.temp_min - 273.15).toFixed(2)}&#176`;
 
-    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" /> <p class="W_stat">${weather[0].main}</p>`
+    weatherMax.innerHTML = `Max : ${(main.temp_max - 273.15).toFixed(2)}&#176`;
 
-    weatherTemp.innerHTML =  `${((main.temp)-273.15).toFixed(2)}&#176`
+    weatherFeelsLike.innerHTML = `${(main.feels_like - 273.15).toFixed(
+      2
+    )}&#176`;
 
-    weatherMin.innerHTML = `Min : ${((main.temp_min)-273.15).toFixed(2)}&#176`
+    weatherHumidity.innerHTML = `${main.humidity}%`;
 
-    weatherMax.innerHTML = `Max : ${((main.temp_max)-273.15).toFixed(2)}&#176`
+    weatherWind.innerHTML = `${(wind.speed * 3.6).toFixed(2)}Km/h`;
 
-    weatherFeelsLike.innerHTML = `${((main.feels_like)-273.15).toFixed(2)}&#176` 
+    weatherPressure.innerHTML = `${main.pressure * 100}Pa`;
+  } catch (error) {
+    alert("Sorry ! Couldn't find that City !");
+  }
+};
 
-    weatherHumidity.innerHTML = `${main.humidity}%`
-
-    weatherWind.innerHTML = `${((wind.speed)*3.6).toFixed(2)}Km/h`
-
-    weatherPressure.innerHTML = `${(main.pressure)*100}Pa`
-
-} catch (error) {
-      alert("Sorry ! Couldn't find that City !")   
-}
-
-}
-
-
-
-loadBody.addEventListener(("load"), fetchApiDataFunc())
+loadBody.addEventListener("load", fetchApiDataFunc());
